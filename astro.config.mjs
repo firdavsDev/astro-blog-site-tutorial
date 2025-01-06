4d5d17e29152ebc06ad5fdfd87fdab64abd70511
firdavsDev <xackercoder@gmail.com>
2025-01-06 15:50:43 +0500

    addd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    redirects: {
+        '/old-page': {
+            status: 302,
+            destination: '/new-page'
+        }
+    }
+});
