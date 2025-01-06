701346c7c9c457ea7a6d322ea66648e4816b5008
firdavsDev <xackercoder@gmail.com>
2025-01-06 16:03:03 +0500

    added some rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
-    redirects: {
-        '/old-page': {
-            status: 302,
-            destination: '/new-page'
-        }
-    }
+  redirects: {
+      '/old-page': {
+          status: 302,
+          destination: '/new-page'
+      }
+  },
+
+  site: "https://example.com",
+  integrations: [preact()]
 });
